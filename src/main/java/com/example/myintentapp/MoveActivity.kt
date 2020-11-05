5e35ac0bfa00ace6a25fc1f8ed82c9b9bc711611
waskithan22 <69247887+waskithan22@users.noreply.github.com>
2020-11-05 18:12:13 +0700

    Add files via upload

---
+++ src/main/java/com/example/myintentapp/MoveActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
